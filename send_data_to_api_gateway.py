04679665495213152dcfb38132edab0ccb5e469d
Ayush Kalani <kalani.ayush@gmail.com>
2022-04-23 01:43:40 -0700

    adding main function

--- send_data_to_api_gateway.py
+++ send_data_to_api_gateway.py
@@ -4,6 +4,11 @@ URL = "https://z6itiyatih.execute-api.us-east-1.amazonaws.com/production/test1"
 
 
 def post_video_to_aws_api_gateway():
-    files = {'file': open('bulk_test2.mov', 'rb')}
+    files = {'file': open('test.jpeg', 'rb')}
     response = requests.post(URL, files=files,  headers={
                              "Content-Type": "image/png", "X-API-Key": API_HEADER_ACCESS_KEY})
+    print(response)
+
+
+if __name__ == "__main__":
+    post_video_to_aws_api_gateway()
