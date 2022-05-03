b5f34bf69b9911774c5086c13ef03a3af5d23036
nakulvaidya <vaidyanakul15@gmail.com>
2022-05-02 19:24:46 -0700

    improved the capture and record logic

--- pi_camera_client.py
+++ pi_camera_client.py
@@ -25,11 +25,12 @@ def post_video_to_s3(video_name):
     except ClientError as e:
         logging.error(e)
 
-def send_frame_to_lambda(video_name):
-    path = "/tmp/"
-    video_file_path = "./"
-    os.system("ffmpeg -i " + str(video_file_path) + video_name + " -r 1 " + str(path) + "image-%3d.jpeg")
-
+def send_frame_to_lambda(image_name):
+    # convert to png
+    rawData = open(image_name + '.data', 'rb').read()
+    imgSize = (160, 160)  # the image size
+    img = Image.frombytes('RGB', imgSize, rawData)
+    img.save(image_name + '.png')
 
 def capture_video():
     camera = PiCamera()
@@ -37,36 +38,18 @@ def capture_video():
     camera.vflip = True
     camera.start_preview()
     time.sleep(2)
-
-    for i in range(2):
-        video_name = "video" + str(i) + ".h264"
+    video_name = "video.h264"
+    camera.start_recording(video_name)
+    for i in range(20):
         image_name = "image" + str(i)
-
-        #record video and capture image at 0.3s
-        camera.start_recording(video_name)
+        #capture image at 0.3s
         camera.wait_recording(0.3)
         camera.capture(image_name + '.data', 'rgb', use_video_port=True)
+        send_frame_to_lambda(image_name)
         camera.wait_recording(0.2)
-        camera.stop_recording()
 
-        #convert to png
-        rawData = open(image_name + '.data', 'rb').read()
-        imgSize = (160, 160)  # the image size
-        img = Image.frombytes('RGB', imgSize, rawData)
-        img.save(image_name + '.png')
-
-        # creating processes
-        p1 = multiprocessing.Process(target=post_video_to_s3, args=(video_name, ))
-        p2 = multiprocessing.Process(target=send_frame_to_lambda, args=(video_name, ))
-
-        # starting processes
-        p1.start()
-        p2.start()
-
-        # wait until processes are finished
-        p1.join()
-        p2.join()
-
+    camera.stop_recording()
+    post_video_to_s3(video_name)
 
 if __name__ == "__main__":
     capture_video()
