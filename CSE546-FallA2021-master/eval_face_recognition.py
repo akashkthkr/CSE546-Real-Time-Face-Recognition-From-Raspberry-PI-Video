396237a0a843a33c397f0e0c2adf88d8f5ccc906
Akash Kant <thakur.akashkant@gmail.com>
2022-05-06 22:51:17 -0700

    Adding to resolve latency

--- CSE546-FallA2021-master/eval_face_recognition.py
+++ CSE546-FallA2021-master/eval_face_recognition.py
@@ -13,37 +13,24 @@ import argparse
 import build_custom_model
 
 
-if __name__ == "__main__":
+def face_recognition(pilImage):
+    labels_dir = "./checkpoint/labels.json"
+    model_path = "./checkpoint/model_vggface2_best.pth"
 
-     parser = argparse.ArgumentParser(description='Evaluate your customized face recognition model')
-     parser.add_argument('--img_path', type=str, default="./data/test_me/val/angelina_jolie/1.png", help='the path of the dataset')
-     args = parser.parse_args()
-     img_path = args.img_path
-     labels_dir = "./checkpoint/labels.json"
-     model_path = "./checkpoint/model_vggface2_best.pth"
+    # read labels
+    with open(labels_dir) as f:
+        labels = json.load(f)
+    print(f"labels: {labels}")
 
+    device = torch.device('cpu')
+    model = build_custom_model.build_model(len(labels)).to(device)
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'))['model'])
+    model.eval()
+    print(f"Best accuracy of the loaded model: {torch.load(model_path, map_location=torch.device('cpu'))['best_acc']}")
 
-     # read labels
-     with open(labels_dir) as f:
-          labels = json.load(f)
-     print(f"labels: {labels}")
-
-
-     device = torch.device('cpu')
-     model = build_custom_model.build_model(len(labels)).to(device)
-     model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'))['model'])
-     model.eval()
-     print(f"Best accuracy of the loaded model: {torch.load(model_path, map_location=torch.device('cpu'))['best_acc']}")
-
-
-     img = Image.open(img_path)
-     img_tensor = transforms.ToTensor()(img).unsqueeze_(0).to(device)
-     outputs = model(img_tensor)
-     _, predicted = torch.max(outputs.data, 1)
-     result = labels[np.array(predicted.cpu())[0]]
-     # print(predicted.data, result)
-
-
-     img_name = img_path.split("/")[-1]
-     img_and_result = f"({img_name}, {result})"
-     print(f"Image and its recognition result is: {img_and_result}")
+    img = pilImage
+    img_tensor = transforms.ToTensor()(img).unsqueeze_(0).to(device)
+    outputs = model(img_tensor)
+    _, predicted = torch.max(outputs.data, 1)
+    result = labels[np.array(predicted.cpu())[0]]
+    return result
--- send_data_to_api_gateway.py
+++ send_data_to_api_gateway.py
@@ -11,8 +11,9 @@ def send_images_to_lambda():
     start_time = time.time()
     image_file_name = 'test_img/ak_30.png'
     img_data = open(image_file_name, 'rb').read()
-    data = base64.b64encode(img_data).decode("utf-8")
-    response = (requests.post(URL, data=data, headers={
+    data = base64.b64encode(img_data)
+    img_as_str = str(data, "utf-8")
+    response = (requests.post(URL, data=img_as_str, headers={
         "Content-Type": "image/png; charset=UTF-8", "X-API-Key": API_HEADER_ACCESS_KEY}))
     latency = time.time() - start_time
     db_data = json.loads(response.text)
